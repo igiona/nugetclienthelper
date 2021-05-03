4b8e4b04c198508cbeba265baeb9e1b3335f2d92
igiona <giona.imperatori@gmail.com>
2021-05-03 07:34:22 +0200

    Try install slnlauncher (#2)
    * Try install slnlauncher
    
    * Allow to set env vars in powershell
    
    * Set var in run command
    
    Co-authored-by: Giona Imperatori <giona.imperatori@sonova.com>

---
+++ Exec.ps1
@@ -0,0 +1,3 @@
+.\SetEnvVars.ps1
+$cmd=$args[0]
+dotnet build
---
+++ SetEnvVars.ps1
@@ -0,0 +1,9 @@
+[System.IO.File]::ReadLines((Resolve-Path ".\SetEnvVars.bat").Path) | ForEach-Object {
+       $ar = $_.Split(" =".ToCharArray())
+       $key=$ar[1]
+       $val=$ar[2]
+       if ($key -and $val) {
+              Write-Output "Setting '$key' to '$val'"
+              [Environment]::SetEnvironmentVariable($ar[1],$ar[2])
+       }
+}
