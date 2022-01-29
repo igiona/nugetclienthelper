aa5f0cacf519a73225cbd6124cfe65eacf8619ea
Giona Imperatori <giona.imperatori@gmail.com>
2022-01-29 17:41:37 +0100

    Hide the Implementation/Compile assembly complexity from the user interface

--- Sources/NugetHelper/NugetPackageType.cs
+++ Sources/NugetHelper/NugetPackageType.cs
@@ -6,9 +6,16 @@ namespace NuGetClientHelper
 {
     public enum NuGetPackageType
     {
-        DotNetImplementationAssembly,
-        DotNetCompileTimeAssembly,
+        /// <summary>
+        /// To be used for packages that follow the NuGet package conventions.
+        /// <see href="https://docs.microsoft.com/en-us/nuget/create-packages/creating-a-package#from-a-convention-based-working-directory" />
+        /// </summary>
+        DotNet,
 
-        Other
+        /// <summary>
+        /// For all packages that do not follow the official conventions.
+        /// <see href="https://docs.microsoft.com/en-us/nuget/create-packages/creating-a-package#from-a-convention-based-working-directory" />
+        /// </summary>
+        Custom
     }
 }
